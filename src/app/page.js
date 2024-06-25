03e7e95324baf71b9ce276585688e8963923ddae
Faraz Maqsood <farazmaqsood.coder@gmail.com>
2024-06-25 18:56:26 +0500

    ada

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Hero from "./Components/Home/Hero";
 import FeaturedProperties from "./Components/FeaturedProperties/FeaturedProperties";
 export default function Home() {
   return (
-    <>
+    <div>
       <Hero />
       <FeaturedProperties />
-    </>
+    </div>
   );
 }
