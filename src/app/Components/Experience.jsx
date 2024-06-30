20b2a7732875a48fdf329dca73039ad779800c7d
Faraz Maqsood <farazmaqsood.coder@gmail.com>
2024-06-30 15:17:23 +0500

    Experience

---
+++ src/app/Components/Experience.jsx
@@ -0,0 +1,86 @@
+export default function Experience() {
+  return (
+    <section id="experience">
+      <div className="max-w-6xl mx-auto my-12 p-3">
+        <h1 className="text-white text-4xl">
+          Navigating the Estatein Experience
+        </h1>
+        <p className="text-gray60">
+          At Estatein, we've designed a straightforward process to help you find
+          and purchase your dream property with ease. Here's a step-by-step
+          guide to how it all works.
+        </p>
+        <div className="grid md:grid-cols-3 md:grid-rows-2 gap-4 grid-cols-1 grid-rows-6 mt-9">
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0 pb-0">
+            <h1 className="text-white p-3 pt-0">Step 01</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">Discover a World of Possibilities</h1>
+              <p className="text-gray60">
+                Your journey begins with exploring our carefully curated
+                property listings. Use our intuitive search tools to filter
+                properties based on your preferences, including location, type,
+                size, and budget.
+              </p>
+            </div>
+          </div>
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0">
+            <h1 className="text-white p-3 pt-0">Step 02</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">Narrowing Down Your Choices</h1>
+              <p className="text-gray60">
+                Once you've found properties that catch your eye, save them to
+                your account or make a shortlist. This allows you to compare and
+                revisit your favorites as you make your decision.
+              </p>
+            </div>
+          </div>
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0">
+            <h1 className="text-white p-3 pt-0">Step 03</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">Personalized Guidance</h1>
+              <p className="text-gray60">
+                Have questions about a property or need more information? Our
+                dedicated team of real estate experts is just a call or message
+                away. 
+              </p>
+            </div>
+          </div>
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0">
+            <h1 className="text-white p-3 pt-0">Step 04</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">See It for Yourself</h1>
+              <p className="text-gray60">
+                Arrange viewings of the properties you're interested in. We'll
+                coordinate with the property owners and accompany you to ensure
+                you get a firsthand look at your potential new home.
+              </p>
+            </div>
+          </div>
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0">
+            <h1 className="text-white p-3 pt-0">Step 05</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">Making Informed Decisions</h1>
+              <p className="text-gray60">
+                Before making an offer, our team will assist you with due
+                diligence, including property inspections, legal checks, and
+                market analysis. We want you to be fully informed and confident
+                in your choice.
+              </p>
+            </div>
+          </div>
+          <div className="border-l-2 border-l-blue-500 p-3 pl-0">
+            <h1 className="text-white p-3 pt-0">Step 06</h1>
+            <div className="text-center p-3 border-[1px] border-blue-500">
+              <h1 className="text-white">Getting the Best Deal</h1>
+              <p className="text-gray60">
+                We'll help you negotiate the best terms and prepare your offer.
+                Our goal is to secure the property at the right price and on
+                favorable terms. 
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "../Components/About/Hero";
 import Values from "../Components/Values";
 import Achievements from "../Components/Achievements";
+import Experience from "../Components/Experience";
 export default function About() {
   return (
     <>
       <Hero />
       <Values />
       <Achievements />
+      <Experience />
     </>
   );
 }
