9749d884119e91bdb0883bbdb7308631b44a389f
Faraz Maqsood <farazmaqsood.coder@gmail.com>
2024-06-30 14:54:21 +0500

    Achievements

---
+++ src/app/Components/Achievements.jsx
@@ -0,0 +1,40 @@
+export default function Achievements() {
+  return (
+    <section id="achievements">
+      <div className="max-w-6xl mx-auto my-12 p-3">
+        <h1 className="text-4xl text-white">Our Achievements</h1>
+        <p className="text-gray60">
+          Our story is one of continuous growth and evolution. We started as a
+          small team with big dreams, determined to create a real estate
+          platform that transcended the ordinary.
+        </p>
+        <div className="flex flex-col md:flex-row gap-4 mt-6">
+          <div className="flex flex-col space-y-5 shadow-sm shadow-gray-500 p-4">
+            <h1 className="text-white text-xl">3+ Years of Experience</h1>
+            <p className="text-gray60">
+              With over 3 years in the industry, we've amassed a wealth of
+              knowledge and experience, becoming a go-to resource for all things
+              real estate.
+            </p>
+          </div>
+          <div className="flex flex-col space-y-5 shadow-sm shadow-gray-500 p-4">
+            <h1 className="text-white text-xl">3+ Years of Experience</h1>
+            <p className="text-gray60">
+              With over 3 years in the industry, we've amassed a wealth of
+              knowledge and experience, becoming a go-to resource for all things
+              real estate.
+            </p>
+          </div>
+          <div className="flex flex-col space-y-5 shadow-sm shadow-gray-500 p-4">
+            <h1 className="text-white text-xl">3+ Years of Experience</h1>
+            <p className="text-gray60">
+              With over 3 years in the industry, we've amassed a wealth of
+              knowledge and experience, becoming a go-to resource for all things
+              real estate.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "../Components/About/Hero";
 import Values from "../Components/Values";
+import Achievements from "../Components/Achievements";
 export default function About() {
   return (
     <>
       <Hero />
       <Values />
+      <Achievements />
     </>
   );
 }
